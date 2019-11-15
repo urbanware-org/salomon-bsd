463c6798e5771df416fab3d0fe6f87e015ef1045
urbanware-org <github@urbanware.org>
2019-11-15 21:49:36 +0100

    Raise version number

--- core/global.sh
+++ core/global.sh
@@ -16,7 +16,7 @@ set_global_variables() {
     # the main directory of SaLoMon for configuration options.
     # ------------------------------------------------------------------------
 
-    version="1.14.2"
+    version="1.15.0"
 
     bsd_name=$(uname)
 
