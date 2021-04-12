485f04d90e916a3ca8e5cc041fdeea9843593d11
urbanware-org <github@urbanware.org>
2021-04-12 21:33:29 +0200

    Move variable to global core module

--- install.sh
+++ install.sh
@@ -25,7 +25,6 @@ target_dir="/opt/salomon-bsd"
 
 clean_install=0
 exclude_config=""
-git_clone=".git snippets wiki README.md"
 keep_directory=0
 target="${cl_yl}${target_dir}${cl_n}"
 yesno="${cl_yl}Y${cl_n}/${cl_yl}N${cl_n}"
